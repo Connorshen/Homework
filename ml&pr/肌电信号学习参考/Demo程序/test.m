fe01f7569f59006905178bd6db5d4e0f3a150a96
沈正圆 <914138410@qq.com>
2019-01-05 16:19:47 +0800

    add

--- ml&pr/肌电信号学习参考/Demo程序/test.m
+++ ml&pr/肌电信号学习参考/Demo程序/test.m
@@ -14,9 +14,9 @@ data2 = data_all(16001:19000,1:16); %第二个手势
 data3 = data_all(26001:29000,1:16); %第三个手势
 data4 = data_all(36001:39000,1:16); %第四个手势
 data5 = data_all(46001:49000,1:16); %第五个手势
-data6 = data_all(56001:59000,1:16); %第六个手势
-data7 = data_all(66001:69000,1:16); %第七个手势
-data8 = data_all(76001:79000,1:16); %第八个手势
+%data6 = data_all(56001:59000,1:16); %第六个手势
+%data7 = data_all(66001:69000,1:16); %第七个手势
+%data8 = data_all(76001:79000,1:16); %第八个手势
 
 %对各个手势动作部分的信号进行RMS信号提取
 data_1_1 = GetRMS(data1, 300, 50);
@@ -24,9 +24,9 @@ data_2_1 = GetRMS(data2, 300, 50);
 data_3_1 = GetRMS(data3, 300, 50);
 data_4_1 = GetRMS(data4, 300, 50);
 data_5_1 = GetRMS(data5, 300, 50);
-data_6_1 = GetRMS(data6, 300, 50);
-data_7_1 = GetRMS(data7, 300, 50);
-data_8_1 = GetRMS(data8, 300, 50);
+%data_6_1 = GetRMS(data6, 300, 50);
+%data_7_1 = GetRMS(data7, 300, 50);
+%data_8_1 = GetRMS(data8, 300, 50);
 
 %对各个手势动作部分的信号标定类别标签
 a1 = ones(55,1) * 1;
@@ -34,9 +34,9 @@ a2 = ones(55,1) * 2;
 a3 = ones(55,1) * 3;
 a4 = ones(55,1) * 4;
 a5 = ones(55,1) * 5;
-a6 = ones(55,1) * 6;
-a7 = ones(55,1) * 7;
-a8 = ones(55,1) * 8;
+%a6 = ones(55,1) * 6;
+%a7 = ones(55,1) * 7;
+%a8 = ones(55,1) * 8;
 
 %形成训练数据+测试数据合集
 data_1 = [data_1_1 a1];
@@ -44,13 +44,13 @@ data_2 = [data_2_1 a2];
 data_3 = [data_3_1 a3];
 data_4 = [data_4_1 a4];
 data_5 = [data_5_1 a5];
-data_6 = [data_6_1 a3];
-data_7 = [data_7_1 a4];
-data_8 = [data_8_1 a5];
+%data_6 = [data_6_1 a3];
+%data_7 = [data_7_1 a4];
+%data_8 = [data_8_1 a5];
 
 %分别按照比例形成训练集和测试集
 [row, col] = size(data_1);
-data_traing = [data_1;data_2;data_3;data_4;data_5;data_6;data_7;data_8];
+data_traing = [data_1;data_2;data_3;data_4];
 [m, n] = size(data_traing);
 num_train = fix(m * 0.7);
 num_test = fix(m * 0.3);
